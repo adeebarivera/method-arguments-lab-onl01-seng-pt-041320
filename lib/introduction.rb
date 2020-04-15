0f98afd4a5b47e671295d963772e5754b336159a
adeeba reaz <adeebarivera@outlook.com>
2020-04-15 20:09:43 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,10 @@
 def introduction (name)
-  puts "Hi, my name is #{name}
+  puts "Hi, my name is #{name}"
   end
-  
-def  introduction_with_language (name,language)
-  puts  "Hi, my name is #{name} and i am learning (language)
-end
-introduction_with_language ("Josh,Ruby")
+ introduction ("Josh")
+ 
+ def introduction_with_language (name,language)
+   puts "Hi,my name is #{name} and i am studying #{language}"
+ end
+ 
+ introduction_with_language ("Michael","Typescript" )
