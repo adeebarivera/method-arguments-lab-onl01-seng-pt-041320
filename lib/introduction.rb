70d935c9b65f2670b2daa5bdb102f86f5d1d2fcc
adeeba reaz <adeebarivera@outlook.com>
2020-04-15 20:43:51 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction (name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
   end
  
  def introduction_with_language (name,language)
-   puts "Hi,my name is #{name} and i am studying #{language}"
+   puts "Hi,my name is #{name} and i am studying #{language}."
  end
  
  introduction_with_language ("Josh","Ruby")
